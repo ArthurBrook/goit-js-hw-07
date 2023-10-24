4ee7cd4a394373a97dc3222420ef13c22a966e94
Arthur Brook <arthurbrook1@gmail.com>
2023-10-24 10:52:59 +0300

    add tack1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,16 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
+
+document.querySelector(".gallery").addEventListener("click", (event) => {
+  if (event.target.tagName === "IMG") {
+    event.preventDefault();
+    const imageUrl = event.target.dataset.source;
+
+    const instance = basicLightbox.create(`
+      <img src="${imageUrl}" width="800" height="600">
+    `);
+
+    instance.show();
+  }
+});
