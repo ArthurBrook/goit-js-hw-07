b02816c8bf0469180e530d38e383a1e66ae15ae3
Arthur Brook <arthurbrook1@gmail.com>
2023-11-02 17:15:23 +0200

    add changes

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,15 +2,53 @@ import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
 
-document.querySelector(".gallery").addEventListener("click", (event) => {
-  if (event.target.tagName === "IMG") {
-    event.preventDefault();
-    const imageUrl = event.target.dataset.source;
+const imageContainer = document.querySelector(".gallery");
+const cardsMarkup = createImageCardsMarkup(galleryItems);
 
-    const instance = basicLightbox.create(`
-      <img src="${imageUrl}" width="800" height="600">
-    `);
+imageContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+imageContainer.addEventListener("click", onImageContainerClick);
 
-    instance.show();
+function createImageCardsMarkup(Items) {
+  return Items.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+  }).join("");
+}
+
+function onImageContainerClick(evt) {
+  evt.preventDefault();
+  const isGalleryImageEl = evt.target.classList.contains("gallery__image");
+  if (!isGalleryImageEl) {
+    return;
   }
-});
+  console.log(evt.target);
+  const instance = basicLightbox.create(
+    `<img src="${evt.target.dataset.source}" width="800" height="600">`,
+    {
+      onShow: () => {
+        window.addEventListener("keydown", onEscClick);
+        console.log("onShow", instance);
+      },
+      onClose: () => {
+        window.removeEventListener("keydown", onEscClick);
+        console.log("onClose", instance);
+      },
+    }
+  );
+
+  instance.show();
+
+  function onEscClick(evt) {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  }
+}
